737b94ddd657025147f601bf631292d6cf91feac
Luis Espinal <lespinalrivera@gmail.com>
2018-03-31 10:29:50 -0700

    Adding image for background and Learn form

---
+++ src/components/Learn.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Learn extends Component {
+    render(){
+        return(
+        <div>
+            <h1>Learn to Code</h1>
+            <p>While helping british columbia's non profits<br/>
+                Thru a project-based learning curriculum</p>
+            <form>
+                <input type="text" name="firstname" value={'First Name'}/>
+                <input type="text" name="lastname" value={'Last Name'}/>
+                <input type="text" name="email" value={'hello@example.com'}/>
+                <input type="text" name="phone number" value={'phone number 604-555-5555'}/>
+                <input type="button" onClick={'submit'} value="Continue Application"/>
+            </form>
+        </div>
+    );
+    }
+    }
+
+    export default Learn;
